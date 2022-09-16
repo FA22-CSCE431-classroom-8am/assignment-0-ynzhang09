1da41c5d67b0010b9b6533f9fcdd8a0ce875be27
Yuning Zhang <yuning.zhang09@gmail.com>
2022-09-15 20:53:09 -0500

    Final Commit

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,9 +4,4 @@ class Book < ApplicationRecord
     validates :price, presence: true
     validates :published_date, presence: true
    
-
-    # attribute :title, :string
-    # attribute :author, :string
-    # attribute :price, :numeric
-    # attribute :published_date, :date
 end
